9f9a5057bfa05a8906c6575b592618c97edc3f53
zuy_szatan <kamillcybulski@gmail.com>
2020-08-29 16:44:50 +0200

    Add basic grid and form

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,58 @@
+<template>
+  <form :class="$style.root" @submit.prevent="createTodo">
+    <h1>Create new item</h1>
+    <div>
+      <div :class="$style.inputWrapper">
+        <ui-textbox floatingLabel label="name" v-model="name" type="text" />
+      </div>
+      <div :class="$style.inputWrapper">
+        <ui-textbox floatingLabel label="notes" v-model="notes" multiLine />
+      </div>
+    </div>
+    <ui-button
+      :class="$style.button"
+      color="primary"
+      buttonType="submit"
+      size="large"
+    >
+      Save
+    </ui-button>
+  </form>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      name: '',
+      notes: '',
+    };
+  },
+  methods: {
+    createTodo() {
+      console.log(this.name, ' | ', this.notes);
+    },
+  },
+};
+</script>
+
+<style lang="scss" module>
+.root {
+  width: 100%;
+  height: 100%;
+  padding: 10px 30px 30px;
+  overflow-y: auto;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: space-between;
+}
+
+.inputWrapper {
+  margin-bottom: 35px;
+}
+
+.button {
+  width: 100%;
+}
+</style>
--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -1,3 +1,41 @@
 <template>
-  <div>This is todos placeholder</div>
+  <div :class="$style.root">
+    <div :class="$style.form">
+      <TodoForm />
+    </div>
+    <div :class="$style.list" />
+  </div>
 </template>
+
+<script>
+import TodoForm from '@/components/TodoForm.vue';
+
+export default {
+  components: { TodoForm },
+};
+</script>
+
+<style lang="scss" module>
+.root {
+  flex: 1;
+  padding: 30px;
+  display: grid;
+  grid-template-columns: 500px 1fr;
+  grid-template-rows: 400px 1fr;
+  column-gap: 20px;
+  row-gap: 20px;
+  grid-template-areas:
+    "form list"
+    ". list"
+}
+
+.form {
+  grid-area: form;
+  background-color: $background-light;
+}
+
+.list {
+  grid-area: list;
+  background-color: $background-light;
+}
+</style>
